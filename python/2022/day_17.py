415ad3e2f67b1a59212cda808bdce7af9e4f4d98
Ian <iansedano@gmail.com>
2022-12-18 07:54:27 +0100

    Python 2012 day 17 still working on finding cycle

--- python/2022/day_17.py
+++ python/2022/day_17.py
@@ -21,12 +21,27 @@ ROCKS = [
 
 
 def main():
+
+    sample_puzzle_input = SAMPLE
+    data = parse(sample_puzzle_input)
+    print()
+    solution1 = part1(data)
+    print(solution1)
+    # assert part1(data) == 3068
+    print("SHOULD BE 3068\n")
+    # solution2 = part2(data)
+    # print(solution2)
+
+    # # raise SystemExit()
+    # print("SHOULD BE 1514285714288\n")
+
     puzzle_input = get_data(day=17, year=2022).strip()
-    puzzle_input = SAMPLE
     data = parse(puzzle_input)
     solution1 = part1(data)
-    solution2 = part2(data)
-
+    print(solution1)
+    print("SHOULD BE 3065\n")
+    # solution2 = part2(data)
+    # print()
     return solution1, solution2
 
 
@@ -38,29 +53,30 @@ def parse(puzzle_input: str):
 def part1(data):
     """Solve part 1."""
 
-    cave = sim(
+    return sim(
         cave_width=7,
         rocks=itertools.cycle(ROCKS),
         jets=itertools.cycle(data),
         number_of_rocks=2022,
     )
 
-    return min(cave)
-
 
 def part2(data):
     """Solve part 2."""
-    cave = sim(
+    return sim(
         cave_width=7,
         rocks=itertools.cycle(ROCKS),
         jets=itertools.cycle(data),
         number_of_rocks=1_000_000_000_000,
     )
 
-    return min(cave)
-
 
 def instantiate_rock(rock, highest_point=0, left_pad=2, bottom_pad=3):
+    """
+    Each rock appears so that its left edge is two units away from the left
+    wall and its bottom edge is three units above the highest rock in the room
+    (or the floor, if there isn't one).
+    """
     return tuple(
         (x + left_pad, y - bottom_pad + highest_point) for x, y in rock
     )
@@ -92,108 +108,101 @@ def push_rock(rock, left, max_width, cave):
             return potential_new_position
 
 
+class CycleDetector:
+    def __init__(self, rock_number, total_rocks):
+        self.starting_rock_number = rock_number
+        self.total_rocks = total_rocks
+        self.log = []
+        self.max_height_log = []
+
+    def input(self, rock_number, high_points, current_rock_shape):
+        fingerprint = (
+            tuple(h - max(high_points) for h in high_points)
+            + current_rock_shape
+        )
+        current_height = min(high_points)
+
+        if fingerprint in self.log:
+            return self.calculate_cycle(
+                fingerprint, rock_number, current_height
+            )
+
+        self.log.append(fingerprint)
+        self.max_height_log.append(min(high_points))
+
+        return False
+
+    def calculate_cycle(self, fingerprint, rock_number, current_height):
+
+        cycle_start = self.log.index(fingerprint)
+        h_at_start_cycle = self.max_height_log[cycle_start]
+        cycle_period = (rock_number - self.starting_rock_number) - cycle_start
+        dh = current_height - h_at_start_cycle
+
+        print(f"{dh = } {cycle_period = }")
+
+        rocks_left = self.total_rocks - rock_number
+        full_cycles_left = rocks_left // cycle_period
+        height_sum = full_cycles_left * dh
+        if rocks_left_over := rocks_left % cycle_period:
+
+            left_over_height = (
+                self.max_height_log[rocks_left_over + cycle_start]
+                - h_at_start_cycle
+            )
+        else:
+            left_over_height = 0
+
+        return -(current_height + height_sum + left_over_height)
+
+
 def sim(
     cave_width, rocks: itertools.cycle, jets: itertools.cycle, number_of_rocks
 ):
-    """
-    Each rock appears so that its left edge is two units away from the left
-    wall and its bottom edge is three units above the highest rock in the room
-    (or the floor, if there isn't one).
-    """
-
     cave = set()
-    highest_points = [0] * cave_width
-    highest_point_log = []
-    now = time.perf_counter()
-    for _ in range(number_of_rocks):
-        if _ % 10_000 == 0:
-            now_ = time.perf_counter()
-            print(_, end="  ")
-            print(now_ - now)
-            now = now_
+    highest_points = (0,) * cave_width
 
-        # print("\n\n============NEW ROCK============\n\n")
-        rock = instantiate_rock(next(rocks), min(highest_points))
-        # viz(cave, rock)
+    cycle_detector = CycleDetector(0, number_of_rocks)
+
+    for rock_number in range(number_of_rocks):
+        if rock_number % 10_000 == 0:
+            print(f"{rock_number = }")
+
+        rock_shape = next(rocks)
+        rock = instantiate_rock(rock_shape, min(highest_points))
         rock = push_rock(rock, next(jets) == "<", cave_width, cave)
-        # viz(cave, rock)
-        at_rest = False
 
+        at_rest = False
         while not at_rest:
             rock = tuple((x, y + 1) for x, y in rock)
-            # viz(cave, rock)
             rock = push_rock(rock, next(jets) == "<", cave_width, cave)
-
-            # viz(cave, rock)
 
             if any((x, y + 1) in cave or y == 0 for x, y in rock):
                 at_rest = True
 
             if at_rest:
-                # print("ROCK AT REST")
-                # viz(cave, rock)
                 for point in rock:
                     cave.add(point)
-                for point in rock:
-                    if point[1] <= highest_points[point[0]]:
-                        highest_points[point[0]] = point[1] - 1
 
-                highest_point_fingerprint = tuple(
-                    height - max(highest_points) for height in highest_points
-                )
+                    new_highest_points = list(highest_points)
+                    if point[1] <= new_highest_points[point[0]]:
+                        new_highest_points[point[0]] = point[1] - 1
+                        highest_points = tuple(new_highest_points)
 
-                if highest_point_fingerprint not in highest_point_log:
-                    # print(tuple(highest_point_fingerprint))
-                    highest_point_log.append(tuple(highest_point_fingerprint))
-                else:
-                    print("FOUND")
-                    print(_)
-                    print(min(highest_points))
-                    print(highest_point_log.index(highest_point_fingerprint))
-                    print(highest_point_fingerprint)
-                    highest_point_log = [highest_point_fingerprint]
+                if result := cycle_detector.input(
+                    rock_number, highest_points, rock_shape
+                ):
+
+                    print(f"{result = }")
+
+                    cycle_detector = CycleDetector(rock_number, number_of_rocks)
+                    cycle_detector.input(
+                        rock_number, highest_points, rock_shape
+                    )
 
                 continue
 
-    return highest_points
-
-
-def map_floor(cave, highest_point):
-
-    print(f"{highest_point = }")
-
-    starting_y = min(
-        y for y in range(0, highest_point - 1, -1) if (0, y) in cave
-    )
-
-    point = (0, starting_y)
-    floor = [point]
-    while True:
-        surrounding_squares = get_squares_around(point)
-        for sq in surrounding_squares:
-            if sq in cave and sq not in floor:
-                floor.append(sq)
-                point = sq
-                break
-
-        else:
-            break
-
-    return floor
-
-
-def get_squares_around(point):
-    # N NE E SE S SW W NW
-    return [
-        (point[0] - 1, point[1] - 1),  # NW
-        (point[0], point[1] - 1),  # N
-        (point[0] + 1, point[1] - 1),  # NE
-        (point[0] + 1, point[1]),  # E
-        (point[0] - 1, point[1]),  # W
-        (point[0] - 1, point[1] + 1),  # SW
-        (point[0], point[1] + 1),  # S
-        (point[0] + 1, point[1] + 1),  # SE
-    ]
+    return -min(highest_points)
 
 
 def viz(cave, rock):
