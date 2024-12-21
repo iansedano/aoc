5e1a58e40b3e0e6e225a3f91348e7305e5e858f6
Ian Currie <iansedano@gmail.com>
2024-12-21 08:51:26 +0100

    grid util

--- python/aoc_tools/grid.py
+++ python/aoc_tools/grid.py
@@ -1,4 +1,9 @@
+from collections import defaultdict
+
 from aoc_tools.vector import Vec2
+
+cardinals_4 = ((), (), (), ())
+cardinals_8 = ((), (), (), ())
 
 
 class Grid2D:
@@ -11,3 +16,27 @@ class Grid2D:
             for x, char in enumerate(line):
                 if char not in ignore:
                     cls.grid[Vec2(x, y)] = char
+
+
+def create_grid_dict_from_string(
+    input, ignore=None, use_tuples=False, collect_chars=False
+):
+    ignore = set() if ignore is None else ignore
+    grid = {}
+
+    if collect_chars:
+        chars = defaultdict(set)
+
+    for y, line in enumerate(input.splitlines()):
+        for x, char in enumerate(line):
+            if char not in ignore:
+                if use_tuples:
+                    grid[(x, y)] = char
+                    if collect_chars:
+                        chars[char].add((x, y))
+                else:
+                    grid[Vec2(x, y)] = char
+                    if collect_chars:
+                        chars[char].add(Vec2(x, y))
+
+    return grid if not collect_chars else grid, chars
